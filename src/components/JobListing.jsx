648d11e6ddde76c1451462be7694f86a650683ab
Olya <o.us@sw-tech.by>
2024-04-01 17:27:36 +0300

    React Router Setup. Create Routes From Elements. Router Provider. Homepage Component/Route. Layouts. Jobs Page Component/Route. Link Component. Custom 404 Page. Active Links With NavLink

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import {FaMapMarker} from 'react-icons/fa'
+import { FaMapMarker } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 
 const JobListing = ({ job }) => {
     const [showFullDescription, setShowFullDescription] = useState(false);
@@ -9,6 +10,7 @@ const JobListing = ({ job }) => {
     if (!showFullDescription) {
       description = description.substring(0, 90) + '...'
     }
+    
     return (
         <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
@@ -27,12 +29,12 @@ const JobListing = ({ job }) => {
                         <FaMapMarker className='inline text-lg mb-1 mr-1'/>
                         {job.location}
                     </div>
-                    <a
-                        href={`/job/${job.id}`}
+                    <Link
+                        to={`/job/${job.id}`}
                         className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                     >
                         Read More
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
