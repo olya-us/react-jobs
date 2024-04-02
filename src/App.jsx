fcda9240ea3693aa6980c7acd407c783615379f3
Olya <o.us@sw-tech.by>
2024-04-02 13:07:36 +0300

    Edit Job Page/Form. Update Form Submission. PUT Request to Update Job

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
-import JobPage, { jobLoader } from './pages/JobPage';
-import AddJobPage from './pages/AddJobPage';
+import JobPage, { jobLoader } from './pages/JobPage'
+import AddJobPage from './pages/AddJobPage'
+import EditJobPage from './pages/EditJobPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 
@@ -43,6 +44,7 @@ const App = () => {
         <Route index element={<HomePage/>} />
         <Route path='/jobs' element={<JobsPage/>} />
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+        <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader}/>
         <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader} />
         <Route path='*' element={<NotFoundPage/>} />
       </Route>
