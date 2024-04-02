82a86094e9e4ddd2249350ef550ee48d2ad1f41f
Olya <o.us@sw-tech.by>
2024-04-02 12:24:19 +0300

    Working With Forms. Form Submission. Pass Function as Prop. POST Request to Add Job. Delete Job Button/function

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,37 @@ import AddJobPage from './pages/AddJobPage';
 import NotFoundPage from './pages/NotFoundPage'
 
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout/>}>
-      <Route index element={<HomePage/>} />
-      <Route path='/jobs' element={<JobsPage/>} />
-      <Route path='/add-job' element={<AddJobPage/>} />
-      <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader} />
-      <Route path='*' element={<NotFoundPage/>} />
-    </Route>
-  )  
-)
-
 const App = () => {
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
+  };
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout/>}>
+        <Route index element={<HomePage/>} />
+        <Route path='/jobs' element={<JobsPage/>} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader} />
+        <Route path='*' element={<NotFoundPage/>} />
+      </Route>
+    )  
+  )
+
   return (
     <RouterProvider router={router}/>
   )
