6b2e8e309fff604dc5dccdb65da2f538d5b0382c
Olya <o.us@sw-tech.by>
2024-04-02 11:35:58 +0300

    Single Job Page. useParams() to Get ID. Data Loaders. Single Job Output. Add Job Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 import NotFoundPage from './pages/NotFoundPage'
 
 
@@ -10,6 +12,8 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout/>}>
       <Route index element={<HomePage/>} />
       <Route path='/jobs' element={<JobsPage/>} />
+      <Route path='/add-job' element={<AddJobPage/>} />
+      <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader} />
       <Route path='*' element={<NotFoundPage/>} />
     </Route>
   )  
